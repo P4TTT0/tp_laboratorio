6496f214674742d2f441e7d7b52986090ee6c94b
Patricio <patricio.perezcardenal@gmail.com>
2022-04-07 11:56:16 -0300

    PRUEBA DE CREACION DE BIBLIOTECA

---
+++ TP_1/src/calcular.h
@@ -0,0 +1,16 @@
+/*
+ * calcular.h
+ *
+ *  Created on: 7 abr. 2022
+ *      Author: patri
+ */
+//.H Para guardar prototipos de funciones
+
+int sumar(void);
+
+#ifndef CALCULAR_H_
+#define CALCULAR_H_
+
+
+
+#endif /* CALCULAR_H_ */
