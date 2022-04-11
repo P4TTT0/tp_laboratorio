17274adfa245d263d692b8e94a9b90df4e583fde
Patricio <patricio.perezcardenal@gmail.com>
2022-04-11 00:39:51 -0300

    TP V-0.01 (OPTION 1 STILL IN PROGRESS)

---
+++ TP_1/src/funciones.h
@@ -0,0 +1,18 @@
+/*
+ * funciones.h
+ *
+ *  Created on: 10 abr. 2022
+ *      Author: Perez Cardenal Patricio
+ */
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+void separador();
+
+void Clear();
+
+int validarNumero(char mensaje [], float numeroMin, float numeroMax);
+
+
+#endif /* FUNCIONES_H_ */
